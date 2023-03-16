6e9db064f8c73ad66dc5324f73fc1cadaf155e58
Kostya Kokhanov <knuckostya@gmail.com>
2023-03-16 16:36:39 +0200

    test

--- src/state/store.ts
+++ src/state/store.ts
@@ -16,3 +16,4 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
+//test
